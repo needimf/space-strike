1eac3f1a2ea62f24bc955894b25e927551bb0dcb
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-10 12:40:22 -0800

    Linked bootstrap CDN and built out simple NavBar component.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+import NavBar from './../../components/NavBar/NavBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  // Event Listeners
+
+
+  // Lifecycle Methods
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <NavBar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
