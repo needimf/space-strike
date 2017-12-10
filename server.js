8a5fb6e91186a0c527dedfd470bf7601b37d9f26
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-09 21:49:47 -0800

    Installed modules to run express server and mongoDB, created server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+// require('./config/database');
+
+app.use(logger('dev'));
+
+// Middleware
+app.use(favicon(path.join(__dirname, 'build', 'favivon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// API routes
+
+// Client-side catch all route
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+let port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
+});
