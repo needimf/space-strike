f120d6ae88dc91732dda55a8297a3dbf2dd7df3c
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-11 17:52:56 -0800

    Created users api routes and controllers files, and stubbed them up for jwt auth.

---
+++ controllers/users.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
+module.exports = {
+  signup,
+  login
+}
+
+function signup(req, res) {
+  let user = new User(req.body);
+  user.save()
+    .then(user => {
+      res.json({token: createJWT(user)});
+    })
+    .catch(err => res.status(400).json(err));
+}
+
+function login(req, res) {
+
+}
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+  return jwt.sign(
+    {user},
+    SECRET,
+    {expiresIn: '24h'}
+  );
+}
