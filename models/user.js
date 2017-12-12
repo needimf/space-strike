80e913ff5b5250a9e902f1055d7f492161502540
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-11 17:46:37 -0800

    Began implementing JSON web token auth in order to be able to create logged in users for game development.

--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-const userSchema = new Schema(
+const SALT_ROUNDS = 6;
+
+let userSchema = new Schema(
   {
     firstName: String,
     lastName: String,
+    email: {type: String, required: true, lowercase: true, unique: true},
+    password: String,
     wins: Number,
     totalGames: Number,
     currentGame: {type: Schema.Types.ObjectId, ref: 'Game'}
@@ -13,5 +18,22 @@ const userSchema = new Schema(
     timestamps: true
   }
 );
+
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    delete ret.password;
+    return ret;
+  }
+});
+
+userSchema.pre('save', function(next) {
+  if (!user.isModified('password')) return next();
+  bcrypt.hash(user.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+
+    this.password = hash;
+    next();
+  })
+})
 
 module.exports = mongoose.model('User', userSchema);
